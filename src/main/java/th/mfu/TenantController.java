4a2e4b0993891ccd30fca4472dfec5f4a50997e8
Kaung Htut Hlaing <6531503145@lamduan.mfu.ac.th>
2023-11-07 21:47:45 +0700

    Update TenantController.java

--- src/main/java/th/mfu/TenantController.java
+++ src/main/java/th/mfu/TenantController.java
@@ -2,13 +2,28 @@ package th.mfu;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class TenantController {
     @PostMapping("/login")
     public String validate()
     {
-        return "home.html";
+        return "redirect:/home";
     }
+    
+    @PostMapping("/register")
+    public String register()
+    {
+        return "redirect:/home";
+    }
+
+   @GetMapping("/home")
+   public String home()
+   {
+    return "home";
+   }
     
 }
